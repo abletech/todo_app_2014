c4e0d19d8a91509fb9c0915ad0f6fb21aca31adc
Nigel Ramsay <nigel.ramsay@abletech.co.nz>
2014-05-14 02:25:28 +0000

    initial commit

---
+++ app/models/todo.rb
@@ -0,0 +1,29 @@
+class Todo < ActiveRecord::Base
+  validates :title, presence: true
+  validates :order, numericality: {greater_than_or_equal_to: 0}, 
+                    presence: true, uniqueness: true
+
+  def self.create_and_initialise(params={})
+    todo = new(params)
+    todo.order = Todo.maximum("order") + 1
+    todo.completed = false
+    todo
+  end
+  
+  def toggle_completed
+    self.completed = !completed
+  end
+
+  def toggle_completed!
+    toggle_completed
+    save!
+  end
+  
+  def self.completed
+    where(completed: true)
+  end
+
+  def self.active
+    where(completed: false)
+  end
+end
---
+++ app/views/todos/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @todo, :id, :title, :order, :completed, :created_at, :updated_at
